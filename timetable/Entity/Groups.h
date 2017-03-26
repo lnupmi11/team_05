75ef57cf9023666520934ed6f495ddc532212965
AlexSkorupsky <alex.skorupskyy@gmail.com>
2017-03-27 01:21:03 +0300

    Update Groups.h

--- timetable/Entity/Groups.h
+++ timetable/Entity/Groups.h
@@ -9,30 +9,13 @@ class Group {
 private:
 	string name; // назва спеціальності ( ПМІ)
 	int number; // група ( 11 -- 15)
-	vector<string> subject; // предмети які вивчають
 public:
-	Group() {
-		name = "";
-		number = 0;
-	}
-	Group(string n, int numb, string s) {
-		name = n;
-		number = numb;
-		subject.push_back(s);
-	}
-	Group(string n, int numb, vector<string> s) {
-		name = n;
-		number = numb;
-		subject = s;
-	}
-	Group(const Group& obj) {
-		name = obj.name;
-		number = obj.number;
-		subject = obj.subject;
-	}
+	Group();
+
+	Group(string n, int numb);
+	Group(const Group& obj);
 	void set_name(string n);
 	string get_name();
 	void set_number(int n);
 	int get_number();
-	void save_group();
 };
