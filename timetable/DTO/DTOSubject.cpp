fe421b6830c2f5137d9ffe6471a36efc0cef0544
maryantutunnyk1999 <maryan.tutunnyk@gmail.com>
2017-03-27 00:29:58 +0300

    Add files via upload

---
+++ timetable/DTO/DTOSubject.cpp
@@ -0,0 +1,44 @@
+#include "DtoSubject.h"
+
+void DTO_Subject:: Save_new_subject(Subject object) 
+{
+	string folder_name = "Subject" ;
+	_mkdir(folder_name.c_str());
+	ofstream out_subject;
+	string subjectrecord = folder_name +"\\subjects.txt";
+	out_subject.open(subjectrecord, ios_base::trunc);
+	out_subject << object.get_course_title() << endl;
+	out_subject.close();
+	cout << "This subject created" << endl;
+}
+void DTO_Subject:: Save_new_subjects(vector <Subject> object) 
+{
+	string folder_name = "Subject" ;
+	_mkdir(folder_name.c_str());
+	ofstream out_subject;
+	string subjectrecord = folder_name +"\\subjects.txt";
+	out_subject.open(subjectrecord, ios_base::trunc);
+	for (int i = 1; i <= object.size(); i++) 
+	{
+		out_subject << object[i-1].get_course_title() << endl;
+	}
+	out_subject.close();
+	cout << "This subjects created" << endl;
+}
+vector<string> DTO_Subject::all_subject_date()
+{
+	vector<string> subject;
+	string way = "Subject\\subjects.txt";
+	string line;
+	ifstream input_subject(way);
+	while (!input_subject.eof()) {
+		getline(input_subject, line);
+		if (line.empty()) {
+			break;
+		}
+		subject.push_back(line);
+	}
+	input_subject.close();
+	return subject;
+}
+
---
+++ timetable/DTO/DtoSubject.h
@@ -0,0 +1,14 @@
+#pragma once
+#ifndef _Save_
+#define _Save_
+#include "../Entity/Subject.h"
+#include <direct.h>
+#include <fstream>
+class DTO_Subject
+{
+public:
+	void Save_new_subject(Subject object);
+	void Save_new_subjects(vector<Subject> object);
+	vector<string> all_subject_date();
+};
+#endif
