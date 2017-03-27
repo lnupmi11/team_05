f6218f202fe76b54a953a2fee829061868dbd867
AlexSkorupsky <alex.skorupskyy@gmail.com>
2017-03-27 09:03:16 +0300

    Update Groups.cpp

--- timetable/Entity/Groups.cpp
+++ timetable/Entity/Groups.cpp
@@ -2,25 +2,16 @@
 
 Group::Group() {
 	name = "";
-	number = 0;
 }
-Group::Group(string n, int numb) {
+Group::Group(string n) {
 	name = n;
-	number = numb;
 }
 Group::Group(const Group& obj) {
 	name = obj.name;
-	number = obj.number;
 }
 void Group::set_name(string n) {
 	name = n;
 }
 string Group::get_name() {
 	return name;
-}
-void Group::set_number(int n) {
-	number = n;
-}
-int Group::get_number() {
-	return number;
 }
