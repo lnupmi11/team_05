f31ebaf7904a2bad4779e7f9fd90a8de7489a8e2
AlexSkorupsky <alex.skorupskyy@gmail.com>
2017-05-14 22:36:03 +0300

    Update Groups.cpp

--- timetable/Entity/Groups.cpp
+++ timetable/Entity/Groups.cpp
@@ -1,12 +1,15 @@
 #include "Groups.h"
 
 Group::Group() {
+	id = "";
 	name = "";
 }
-Group::Group(string n) {
+Group::Group(string n, string i) {
+	id = i;
 	name = n;
 }
 Group::Group(const Group& obj) {
+	id = obj.id;
 	name = obj.name;
 }
 void Group::set_name(string n) {
@@ -14,4 +17,10 @@ void Group::set_name(string n) {
 }
 string Group::get_name() {
 	return name;
+}
+void Group::set_id(string i) {
+	id = i;
+}
+string Group::get_id() {
+	return id;
 }
