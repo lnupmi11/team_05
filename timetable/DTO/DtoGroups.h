e2f9066278cd7dc712bd4103e6f1c6e4da1ba2ba
AlexSkorupsky <alex.skorupskyy@gmail.com>
2017-05-14 22:36:38 +0300

    Update DtoGroups.h

--- timetable/DTO/DtoGroups.h
+++ timetable/DTO/DtoGroups.h
@@ -1,14 +1,21 @@
 #pragma once
-#ifndef _Save_
-#define _Save_
+#ifndef _DTOGROUPS_H_
+#define _DTOGROUPS_H_
 #include "../Entity/Groups.h"
 #include <direct.h>
 #include <fstream>
+#include <string>
+using namespace std;
+
 class DTO_Group
 {
 public:
-	void Save_new_group(Group object);
-	void Save_new_group(vector<Group> object);
-	vector<string> all_group_date();
+	void save_new_group_date(Group obj);
+	void create_new_group(Group obj);
+	bool is_group(string id);
+	Group find_date_group(string id);
+	vector<Group> all_group_date();
+	void delete_group(string id);
 };
+
 #endif
