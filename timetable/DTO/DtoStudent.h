a9c635518a41616d7b8f7ef3575037cee7bfcdf7
олег дацків < >
2017-05-20 16:47:47 +0300

    changes turned back

--- timetable/DTO/DtoStudent.h
+++ timetable/DTO/DtoStudent.h
@@ -1,16 +1,20 @@
-#pragma once
-#include <fstream>
-#include <string>
-#include <direct.h>
-#include "../Entity/Student.h"
-
-using namespace std;
-
-class DTO_Student{
-public:
-	void create_new_student(Student object);
-	void save_new_student_data(Student object);
-	Student find_student(string name , string surname);
-	vector<Student> all_student_date();
-	void delete_student(string name , string surname); 
-};
+ #pragma once
+ #ifndef _DTOSTUDENT_H_
+ #define _DTOSTUDENT_H_
+ #include "../Entity/Student.h"
+ #include <direct.h>
+ #include <fstream>
+ #include <string>
+ using namespace std;
+ 
+ class DTO_Student
+ {
+ public:
+ 	void save_new_student_date(Student obj);
+ 	void create_new_student(Student obj);
+ 	bool is_student(string id);
+ 	Student find_date_student(string id);
+ 	vector<Student> all_student_date();
+ 	void delete_student(string id);
+ };
+ #endif 
